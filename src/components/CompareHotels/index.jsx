89f6d5abdb472b44b81c2984f6d87b0c4fd8b785
Gizem Deniz <gizem.deniz@GizemDeniz.home>
2021-12-18 22:56:39 +0300

    compare hotel added

--- src/components/CompareHotels/index.jsx
+++ src/components/CompareHotels/index.jsx
@@ -31,21 +31,33 @@ function CompareHotels() {
 
   return <>
    <Container style={{ padding:'4em', width:"100%"}} fluid="md">
-   <h1>Populer Oteller</h1>
    <Row style={{ justifyContent:'space-between'}}>
   
    <div className="compareHotels">
-    
      {Object.values(data1).map(item => (
       <div className="hotelDetails2">
-        <h5>{item.name}</h5>
-        <h5>{item.starRating}</h5>
-  <ol>
-  <li>Tea</li>
-  <li>Milk</li>
-</ol>
+         <h2 style={{ textAlign: 'center'}}>{item.name}</h2>
       </div>
+      
 ))}
+  <Card style={{ width: '30rem', marginLeft:"1em" }}>
+      <Card.Img variant="top" src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/311927379.jpg?k=16a2e50c992ff849c2639bed280692874e375287399f1e6e938241ce97c4cf3c&o=&hp=1" />
+      <Card.Body>
+      <Card.Title>Amsterdam</Card.Title>
+      <a  style={{ margin: "2em 1em", padding: "1em", fontSize:"12px", borderRadius:"10em" }} href="#" class="badge badge-primary">9.1</a>
+      <a  style={{ margin: "2em 0", padding: "1em", fontSize:"12px", borderRadius:"10em" }} href="#" class="badge badge-success">'COVID-19: Hygiene and cleanliness</a>
+        <ListGroup></ListGroup>
+        <ListGroup>
+        <ListGroup.Item disabled>Ücretsiz Wifi</ListGroup.Item>
+  <ListGroup.Item>Günlük oda temizleme</ListGroup.Item>
+  <ListGroup.Item>24 saat açık resepsiyon</ListGroup.Item>
+  <ListGroup.Item>International Personel</ListGroup.Item>
+</ListGroup>
+<br></br>
+        <Button variant="primary">Standart Oda: 90 € / Gecelik</Button>
+      </Card.Body>
+    </Card>
+
  </div>
 
 
@@ -54,14 +66,31 @@ function CompareHotels() {
      {Object.values(data2).map(item => (
      <div className="w3-card-2">
        <div className="w3-container">
-         <h5>{item.name}</h5>
-         <h5>{item.starRating}</h5>
-
+       <h2 style={{ textAlign: 'center'}}>{item.name}</h2>
        </div>
      </div>
- 
+
 
 ))}
+
+<Card style={{ width: '30rem', marginLeft:"1em" }}>
+      <Card.Img  style={{height: "22em"}}variant="top" src="https://room-matehotels.com/images/img/aitana/hotel/galeria/aitana_gal-4.jpg" />
+      <Card.Body>
+      <Card.Title>Amsterdam</Card.Title>
+      <a  style={{ margin: "2em 1em", padding: "1em", fontSize:"12px", borderRadius:"10em" }} href="#" class="badge badge-primary">8.9</a>
+      <a  style={{ margin: "2em 0", padding: "1em", fontSize:"12px", borderRadius:"10em" }} href="#" class="badge badge-success">'COVID-19: Hygiene and cleanliness</a>
+        <ListGroup></ListGroup>
+        <ListGroup>
+  <ListGroup.Item disabled>Ücretsiz Wifi</ListGroup.Item>
+  <ListGroup.Item>Günlük oda temizleme</ListGroup.Item>
+  <ListGroup.Item>24 saat açık resepsiyon</ListGroup.Item>
+  <ListGroup.Item>İş Oteli</ListGroup.Item>
+</ListGroup>
+<br></br>
+        <Button variant="primary">Standart Oda: 60 € / Gecelik</Button>
+      </Card.Body>
+    </Card>
+
   </div>
 </Row>
 </Container>
